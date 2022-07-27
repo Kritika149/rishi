4fb82664bbbc2230afb0a1431d677f5cb660538f
Kritika <kritikadeo29@gmail.com>
2022-07-27 18:33:57 +0545

    :zap:decorators1

---
+++ builtin_function2.py
@@ -0,0 +1,54 @@
+# def any_fun(name):
+#     return len(name)
+
+# names=["Apple","joy","om","ram kumar"]
+# names2=[1,5,9,8,3,4]
+
+# print(max(names,key=any_fun))
+# print(min(names,key=any_fun))
+# print(max(names2))
+# print(min(names2))
+
+# print(max(names,key= lambda name: len(name)))
+# print(min(names,key= lambda name: len(name)))
+
+#find the name of highest marks:
+# student1 = [
+#     {"name":"Ram","score":60,"age":19},
+#     {"name":"kiran","score":70,"age":20},
+#     {"name":"om","score":80,"age":21}
+#     ]
+
+# student2 = {
+#     "Ram":{"score":60,"age":19},
+#     "kiran":{"score":70,"age":20},
+#     "om":{"score":80,"age":21}
+#     }
+
+# def highest_marks(list):
+#     marks=[]
+#     for each in list:
+#         mark1=each.get("score")
+#         marks.append(mark1)
+#     return max(marks)
+        
+# print(highest_marks(student1))
+
+# print(max(student1,key=lambda dict_item: dict_item.get("score"))["name"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
